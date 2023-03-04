791a459d7d3a3c99452e5baf441cbee0db86e12d
Nick Ballard <znhihgiasy@gmail.com>
2023-03-04 15:15:18 +0800

    :(:(

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-      nicpkgs = {
-        url = "github:nicball/nicpkgs";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      emacs-overlay.url = "github:nix-community/emacs-overlay";
+    nicpkgs = {
+      url = "github:nicball/nicpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
   outputs = { self, nixpkgs, nicpkgs, emacs-overlay, ... }: {
     nixosConfigurations.nicball-nixos = nixpkgs.lib.nixosSystem rec {
