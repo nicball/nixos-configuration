165dd3bda7be96a44146f0a8ed087f14ddf9140b
Nick Ballard <znhihgiasy@gmail.com>
2024-01-20 00:12:19 +0800

    TODO: Replace placeholder code

---
+++ cachix/nicpkgs.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://nicpkgs.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nicpkgs.cachix.org-1:OTCMJ8lLYwhnDhlkP0huok3hOnxV3u/YVDH9M0kPLqM="
+      ];
+    };
+  };
+}
