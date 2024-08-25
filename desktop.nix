8868d722fce01dd182cc2da683cbb7a4426a1a68
Nick Ballard <znhihgiasy@gmail.com>
2024-08-25 15:00:00 +0800

    Added missing file in previous commit

--- desktop.nix
+++ desktop.nix
@@ -63,7 +63,7 @@
   };
 
   # kde-connect
-  programs.kdeconnect.enable = true;
+  # programs.kdeconnect.enable = true;
 
   # Input methods
   i18n.inputMethod = {
@@ -106,16 +106,16 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
-    wireplumber.configPackages = [
-      (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-        bluez_monitor.properties = {
-          ["bluez5.enable-sbc-xq"] = true,
-          ["bluez5.enable-msbc"] = true,
-          ["bluez5.enable-hw-volume"] = true,
-          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        }
-      '')
-    ];
+    # wireplumber.configPackages = [
+    #   (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+    #     bluez_monitor.properties = {
+    #       ["bluez5.enable-sbc-xq"] = true,
+    #       ["bluez5.enable-msbc"] = true,
+    #       ["bluez5.enable-hw-volume"] = true,
+    #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+    #     }
+    #   '')
+    # ];
   };
   hardware.bluetooth = {
     enable = true;
